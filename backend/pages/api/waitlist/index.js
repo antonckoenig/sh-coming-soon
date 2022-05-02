c14576e97f591d32bf0a28dad7a3e9ab7ca5f345
dg314 <david_grossman@brown.edu>
2022-05-01 21:34:10 -0400

    next cors

--- backend/pages/api/waitlist/index.js
+++ backend/pages/api/waitlist/index.js
@@ -2,16 +2,16 @@ import NextCors from 'nextjs-cors';
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).send({ message: `Invalid method: ${req.method}. Only POST requests allowed.`});
-    }
-    // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
     await NextCors(req, res, {
         // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
         origin: '*',
         optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
+
+    if (req.method !== 'POST') {
+        return res.status(405).send({ message: `Invalid method: ${req.method}. Only POST requests allowed.`});
+    }
 
     const email = req.body.email;
 
